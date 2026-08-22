d919849420fe4a24b6388609cc81ce288fcc4fcb
thrushaps <thrushaps@gmail.com>
2026-08-22 22:45:33 +0530

    check div by digit sum and product

---
+++ Check _Divisibility_by_Digit_Sum_and_Product.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        temp = n
+        digit_sum = 0
+        digit_product = 1
+
+        while temp > 0:
+            digit = temp % 10
+            digit_sum += digit
+            digit_product *= digit
+            temp //= 10
+
+        return n % (digit_sum + digit_product) == 0
