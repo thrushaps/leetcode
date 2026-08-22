e4798a0d1d2d7c65abfd9f8beac3c06c1c268150
thrushaps <thrushaps@gmail.com>
2026-08-22 22:47:55 +0530

    logest substring without duplicate

---
+++ longest_substring_without_dup.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = 0
+        longest = 0
+        seen = set()
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+
+            seen.add(s[right])
+            longest = max(longest, right - left + 1)
+
+        return longest
