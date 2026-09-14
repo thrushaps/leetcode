3dca7bd7b18671e1faa69551f12c19b2da047dfd
thrushaps <thrushaps@gmail.com>
2026-09-14 19:11:00 +0530

    add rec-overlap

---
+++ rec_overlap.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isRectangleOverlap(self, rec1: list[int], rec2: list[int]) -> bool:
+        return not (
+            rec1[2] <= rec2[0] or
+            rec1[0] >= rec2[2] or
+            rec1[3] <= rec2[1] or
+            rec1[1] >= rec2[3]
+        )
